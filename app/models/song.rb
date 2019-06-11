dbf5ead6d97e8e83fde92e651129974252403a6f
Matteo Muscas <thatman.j@gmail.com>
2019-06-11 08:39:21 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def song_artist_name=(name)
+     self.artist = Artist.find_or_create_by(name: name)
+   end
+
+   def song_artist_name
+      self.category ? self.category.name : nil
+   end
+
 end
