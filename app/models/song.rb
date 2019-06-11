87cedbea3843faa14e899149c4ebef7f169a0dd8
Matteo Muscas <thatman.j@gmail.com>
2019-06-11 08:57:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,7 +12,7 @@ class Song < ActiveRecord::Base
    end
 
    def genre_name=(name)
-      self.artist = Genre.find_or_create_by(name: name)
+      self.genre = Genre.find_or_create_by(name: name)
     end
 
     def genre_name
